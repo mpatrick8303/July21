5933a709fea879555d67f02e06f2051c54bc1c77
Michael Patrick <mpatrick8303@gmail.com>
2016-07-21 14:02:40 -0400

    fixed i

--- src/com/patrick/FizzBuzz.java
+++ src/com/patrick/FizzBuzz.java
@@ -7,94 +7,105 @@ public class FizzBuzz
 	{
 		
 		
-//		for(int i = 1; i <= 110; i++)
-//		{
-//			System.out.print(i);
-//			
-//			if((i % 3) == 0)
-//			{
-//				System.out.print(" Fizz");
-//			}
-//			
-//			if((i % 5) == 0)
-//			{
-//				System.out.print(" Buzz");
-//				
-//			}
-//			if((i % 7) == 0)
-//			{
-//				System.out.print(" Bang");
-//			}
-//			System.out.println();
-//		}
+		for(int i = 1; i <= 110; i++)
+		{
 			
-// this code would work for 15 and under but would need to check multiples to make this work for higher numbers
-//			for (int i = 1; i <= 110; i++)
-//			{
-//				System.out.print(i);
-//				
-//				if((i % 15) == 0)
-//				{
-//					System.out.print(" Fizz Buzz");
-//				}
-//				else if((i % 7) == 0)
-//				{
-//					System.out.print(" Bang");
-//				}
-//				else if((i % 5) == 0)
-//				{
-//					 
-//					System.out.print(" Buzz");
-//					
-//				}
-//				else if((i % 3) == 0)
-//				{
-//					System.out.print(" Fizz");
-//					
-//				}
-//				System.out.println();
-//				
-//			}
-		
-		
-			for (int i = 110; i > 0; i--)
+			
+			if((i % 3) == 0)
 			{
-				
-				System.out.print(i);
-				
-				if(((i%3)==0) && ((i%5)==0) && ((i%7)==0))
-				{
-					System.out.print(" Fizz Buzz Bang");
-				}
-				else if(((i%3)==0) && ((i%5) == 0))
-				{
-					System.out.print(" Fizz Buzz");
-				}
-				else if(((i%3)== 0) && ((i%7)== 0))
-				{
-					System.out.print(" Fizz Bang");
-				}
-				else if(((i%5)== 0) && ((i%7)== 0))
-				{
-					System.out.print(" Buzz Bang");
-				}
-				else if((i%3)==0)
-				{
-					System.out.print(" Buzz");
-				}
-				else if((i%5) == 0)
-				{
-					System.out.print(" Buzz");
-				}
-				else if((i%7) == 0)
-				{
-					System.out.print(" Bang");
-				}
-				
-				System.out.println();
+				System.out.print(" Fizz");
 			}
 			
 			
+			if((i % 5) == 0)
+			{
+				System.out.print(" Buzz");
+				
+			}
+			
+			if((i % 7) == 0)
+			{
+				System.out.print(" Bang");
+			}
+			
+			if(!((i%7)==0) && !((i%5)== 0) && !((i%3)== 0))
+			{
+				System.out.print(i);
+			}
+			System.out.println();
+		}
+					
+		
+//			for (int i = 110; i > 0; i--)
+//			{
+//				
+//				
+//				
+//				if(((i%3)==0) && ((i%5)==0) && ((i%7)==0))
+//				{
+//					System.out.print(" Fizz Buzz Bang");
+//				}
+//				else if(((i%3)==0) && ((i%5) == 0))
+//				{
+//					System.out.print(" Fizz Buzz");
+//				}
+//				else if(((i%3)== 0) && ((i%7)== 0))
+//				{
+//					System.out.print(" Fizz Bang");
+//				}
+//				else if(((i%5)== 0) && ((i%7)== 0))
+//				{
+//					System.out.print(" Buzz Bang");
+//				}
+//				else if((i%3)==0)
+//				{
+//					System.out.print(" Buzz");
+//				}
+//				else if((i%5) == 0)
+//				{
+//					System.out.print(" Buzz");
+//				}
+//				else if((i%7) == 0)
+//				{
+//					System.out.print(" Bang");
+//				}
+//				else
+//				{
+//					System.out.print(i);
+//				}
+//				
+//				System.out.println();
+//				
+//			}
+//			
+
+		// this code would work for 15 and under but would need to check multiples to make this work for higher numbers
+//		for (int i = 1; i <= 110; i++)
+//		{
+//			System.out.print(i);
+//			
+//			if((i % 15) == 0)
+//			{
+//				System.out.print(" Fizz Buzz");
+//			}
+//			else if((i % 7) == 0)
+//			{
+//				System.out.print(" Bang");
+//			}
+//			else if((i % 5) == 0)
+//			{
+//				 
+//				System.out.print(" Buzz");
+//				
+//			}
+//			else if((i % 3) == 0)
+//			{
+//				System.out.print(" Fizz");
+//				
+//			}
+//			System.out.println();
+//			
+//		}
 		
 
 	}
